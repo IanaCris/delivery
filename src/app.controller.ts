4b790dbacf471575c036ce4d9ee14f6da9b245a5
IanaCris <ianasousati@gmail.com>
2022-01-08 00:35:40 -0300

    route post create client

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateClientService } from './service/createClient.service';
+
+interface CreateClientDto {
+  username: string;
+  password: string;
+}
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly createClient: CreateClientService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post()
+  async createClientNow(@Body() { username, password }: CreateClientDto): Promise<void> {
+    console.log({ username, password });
+    await this.createClient.execute({ username, password });
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CreateClientService } from './service/createClient.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    CreateClientService
+  ],
 })
 export class AppModule {}
