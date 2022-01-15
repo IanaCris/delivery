5ea172c798686da7c37deaa9e96ef58d91eb5335
IanaCris <ianasousati@gmail.com>
2022-01-15 00:20:41 -0300

    generate token jwt

--- src/service/authenticateUser.service.ts
+++ src/service/authenticateUser.service.ts
@@ -1,5 +1,7 @@
 import { prisma } from '../database/prismaClient';
+import { compare } from "bcrypt";
 import { Injectable } from '@nestjs/common';
+import { sign } from "jsonwebtoken";
 
 interface IAutheticateClient {
   username: string;
@@ -9,18 +11,31 @@ interface IAutheticateClient {
 @Injectable()
 export class AutheticateUserService {
   async execute({ username, password }: IAutheticateClient) {
-  
-    //receber username, password
-  
+    
     //verificar se username cadastrado
     const client = await prisma.clients.findFirst({
       where: {
         username
       }
     });
+
+    if(!client) {
+      throw new Error("Username or password invalid!");
+    }
     
     //verificar se senha corresponde ao username
-  
+    const passwordMatch = await compare(password, client.password)
+
+    if(!passwordMatch) {
+      throw new Error("Username or password invalid!");
+    }
+
     //gerar o token
+    const token = sign({username},"1e0fb489cc4e23e43adde0c56c2bc2b0", {
+      subject: client.id,
+      expiresIn: "1d",
+    });
+
+    return token;
   }
 }
